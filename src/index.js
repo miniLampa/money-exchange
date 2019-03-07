d5e9590275c16a542a10a9f68939321c6517328b
Natalia Litvinok <minilampa@gmail.com>
2019-03-07 11:47:02 +0300

    feat: add solution for money exchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+  
+  if (currency > 10000 ){
+    return { error: "You are rich, my friend! We don't have so much coins for exchange" };
+  }
+
+  let result = {};
+  let money = {
+    "H": 50 ,
+    "Q": 25 ,
+    "D": 10 ,
+    "N": 5 ,
+    "P": 1
+  };
+
+  if ( currency > 0){
+    let incomingMoney = currency;
+
+    while ( incomingMoney > 0 ){
+      for (let key in money){
+        if (Math.floor(incomingMoney / money[key]) > 0 ){
+          result[key] = Math.floor(incomingMoney / money[key]);
+        }
+        incomingMoney = incomingMoney % money[key];
+      }
+    }
+  }
+
+  return result;
 }
